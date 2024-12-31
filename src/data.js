72e8153ec3e24bc205bd4fafdd9fabc051404863
vineetuppal <vineet.ai20@bmsce.ac.in>
2024-12-31 18:43:31 +0530

    initial commit

---
+++ src/data.js
@@ -0,0 +1,25 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Vineet Uppal",
+    job: "Software Engineer",
+    image: "https://media.licdn.com/dms/image/v2/D5603AQEi9THmPeBbdg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1716441157352?e=1741219200&v=beta&t=9t52eHqqbxADQPWOSNXrUilaAFwKPLtdw-zJ7zn3nZM",
+    text: "Passionate software developer focused on creating innovative and impactful solutions. Always eager to learn and contribute to cutting-edge technology projects.",
+  },
+  {
+    id: 2,
+    name: "Amogh Nadkarni",
+    job: "Software Engineer",
+    image: "https://media.licdn.com/dms/image/v2/D5603AQHbEXqtxIgR7w/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1707758646336?e=1741219200&v=beta&t=_OFrUt12QS4GoGmP3VNH1xHPcxcQMJdFume8GxU7ddc",
+    text: "I am an aspiring Full-Stack Web Developer with a passion for coding. I am eager to find opportunities where I can grow my skills and make meaningful contributions.",
+  },
+  {
+    id: 3,
+    name: "Harsh kumar",
+    job: "Software Engineer",
+    image: "https://media.licdn.com/dms/image/v2/D5603AQGc_GIZc4lv7Q/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1670594370428?e=1741219200&v=beta&t=nMr1uQOQv4UR9hToRvyXlNwFnHlDSzY2jWimYN3nYek",
+    text: "In my current role at F5, I design and develop scalable distributed systems and microservices, leveraging my expertise in languages such as Go, C++, and query languages like PromQL.",
+  },
+];
+
+export default reviews;
